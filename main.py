24ce348c124ab7a880b92472e5e61178c5219483
Dhani_Baksh <generativeaiandchatbot@gmail.com>
2026-09-24 16:32:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from schemas import UserQuery
 
 app = FastAPI(title="Autonomous-research-agent")
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/research")
 def research(request : UserQuery):
